031aa1bb7dc4a1835b23e60adc1234f46b985f72
klkjjj4848 <fwangjuwon@naver.com>
2022-03-21 17:41:51 +0900

    4. Repository, Service, Controller, ApiController 구현완료

--- src/main/java/site/metacoding/blogv2/domain/post/Post.java
+++ src/main/java/site/metacoding/blogv2/domain/post/Post.java
@@ -8,12 +8,12 @@ import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 

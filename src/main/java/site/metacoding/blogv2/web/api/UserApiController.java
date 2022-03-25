4c89adf17531ca25a60ac70f2075fd8a002a3d5d
a01048197443 <a01048197443@gmail.com>
2022-03-25 10:54:19 +0900

    9. 인증 프로세스 -네비게이션변경, 로그아웃 구현완료

--- src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
@@ -3,6 +3,7 @@ package site.metacoding.blogv2.web.api;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;

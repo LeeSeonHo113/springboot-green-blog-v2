32056d53506bd79b96745d608c82595b2b5fe34d
a01048197443 <a01048197443@gmail.com>
2022-03-28 10:29:16 +0900

    [회원수정 세션값 변경 처리]

---
+++ src/main/java/site/metacoding/blogv2/config/FilterConfig.java
@@ -0,0 +1,13 @@
+package site.metacoding.blogv2.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Configuration;
+
+// Controller, RestController, Repository, Service, Component, Configuration
+
+@Configuration
+public class FilterConfig {
+    public FilterRegistrationBean<?> filter1() {
+        return null;
+    }
+}

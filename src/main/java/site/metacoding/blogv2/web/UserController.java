7735334a3c5644c27127a23c2d06bff4c93e4b28
klkjjj4848 <fwangjuwon@naver.com>
2022-03-31 10:32:57 +0900

    [회원정보보기  CSR로 변경하기]

--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -7,8 +7,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.UserService;
 
 @RequiredArgsConstructor
@@ -16,6 +18,15 @@ import site.metacoding.blogv2.service.UserService;
 public class UserController {
     private final UserService userService;
     private final HttpSession session;
+
+    // 앱은 이 친구 요청 할까? 아니!!
+    // 웹은 이 친구 요청해야함.
+    @GetMapping("/s/user/{id}")
+    public String userInfo(@PathVariable Integer id, Model model) {
+        User userEntity = userService.회원정보(id);
+        model.addAttribute("user", userEntity);
+        return "user/updateForm";
+    }
 
     // @GetMapping("/logout")
     // public String logout() {
